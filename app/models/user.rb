0715e2a91b40dded75df9ad10618d86796837acb
Synkevych Roman <sunkevu4@gmail.com>
2020-04-09 23:48:42 +0300

    style: update style of navbar avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
   validates :username, presence: true, uniqueness: true
-  validates :password, length: { minimum: 3}, :if => :password
   attribute :role, :string, default: 'user'
   attribute :avatar, default: 'https://res.cloudinary.com/demo/image/upload/d_avatar.png/non_existing_id.png'
   
   has_many :aticles, dependent: :destroy
+
+  validates :password, length: { minimum: 3}, :if => :password
 end
